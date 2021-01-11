83c27ae37c5bb870fa56acff3da5a315fa7185a4
skylinecas <jeffchen9639@granitesd.org>
2021-01-10 17:19:27 -0700

    rips all the data necessary

--- championStats.py
+++ championStats.py
@@ -4,6 +4,7 @@
 #
 import requests
 import json
+import random
 
 #printing json files. 
 def jprint(obj):
@@ -25,8 +26,6 @@ championData = requests.get(championLinks)
 championDicts = championData.json()['data']
 #print(championDicts)
 
-Aatrox = championDicts['Aatrox']
-#print(Aatrox)
 
 
 #that's nice, but can we get more information?
@@ -34,9 +33,117 @@ championName = "Aatrox"
 championLink = 'http://ddragon.leagueoflegends.com/cdn/' + latestVersion + '/data/en_US/champion/' + championName + '.json'
 
 championData = requests.get(championLink)
-print(championData.json()['data'][championName])
+#print(championData.json()['data'][championName])
 
 stuff = []
 for key in championData.json()['data'][championName]:
     stuff.append(key)
 print(stuff)
+
+
+"""
+#chioces:
+# ['id', 'key', 'name', 'title', 'image', 'skins', 'lore', 'blurb', 'allytips', 'enemytips', 'tags', 'partype', 'info', 'stats', 'spells', 'passive', 'recommended']
+print(championData.json()['data'][championName]['id']) #aatrox+
+print(championData.json()['data'][championName]['key']) #206
+print(championData.json()['data'][championName]['name']) #aatrox
+print(championData.json()['data'][championName]['title']) #the Darkin Blade+
+print(championData.json()['data'][championName]['image']) #image is useless imo
+print(championData.json()['data'][championName]['skins']) #get the numbers of each skin+
+print(championData.json()['data'][championName]['lore']) #lore+
+print(championData.json()['data'][championName]['blurb']) #blurb
+print(championData.json()['data'][championName]['allytips']) #+
+print(championData.json()['data'][championName]['enemytips'])
+print(championData.json()['data'][championName]['tags']) #fighter, tank+
+print(championData.json()['data'][championName]['partype'])# ?? "Blood Well"
+print(championData.json()['data'][championName]['info']) #??, difficulty
+print(championData.json()['data'][championName]['stats']) #base stats, stats per level, +
+jprint(championData.json()['data'][championName]['spells']) #needs more analysis
+print(championData.json()['data'][championName]['passive']) #passive
+print(championData.json()['data'][championName]['recommended']) #? useless??
+"""
+
+while True:
+    championName = input("Champion Name: ").title()
+    # ISSUE: doesn't work for lee sin.
+    
+    if championName not in championDicts:
+        print("Name not in database, try again. ")
+        continue
+    print("searching.... hold on")
+
+    #creates an API link based on the lastestVersion global variable and the championName that the user inputted. 
+    championLink = 'http://ddragon.leagueoflegends.com/cdn/' + latestVersion + '/data/en_US/champion/' + championName + '.json'
+
+    championData = requests.get(championLink)
+    if (championData.status_code) != 200:
+        print("Something is wrong, please try again.")
+        continue
+
+    championName = championData.json()['data'][championName]['id']
+    championTitle = championData.json()['data'][championName]['title']
+
+    championSkinDict = championData.json()['data'][championName]['skins']
+    championSkinNums = []
+    for dictionary in championSkinDict:
+        championSkinNums.append(dictionary['num'])
+    
+    championLore = championData.json()['data'][championName]['lore']
+
+    try:
+        championTip = random.choice(championData.json()['data'][championName]['allytips'])
+    except:
+        championTip = "Nvm, don't have one XD"
+    
+
+    championAttributesList = championData.json()['data'][championName]['tags']
+    championAttributes = championAttributesList[0]
+    for i in range(1, len(championAttributesList)):
+        championAttributes = championAttributes + "/" + championAttributesList[i]
+    #print(f"{championName} is a {championAttributes}")
+    
+    championDifficulty = championData.json()['data'][championName]['info']['difficulty']
+    #print("Champion Difficulty: " + str(championDifficulty) + "/10")
+
+    championStats = championData.json()['data'][championName]['stats']
+    #print(championStats)
+
+    baseHP = championStats['hp']
+    hpperlevel = championStats['hpperlevel']
+
+    baseMP = championStats['mp']
+    mpperlevel = championStats['mpperlevel']
+
+    armor = championStats['armor']
+    amorperlevel = championStats['armorperlevel']
+
+    magicresist = championStats['spellblock']
+    magicresisterperlevel = championStats['spellblockperlevel']
+
+    hpregen = championStats['hpregen']
+    hpregenperlevel = championStats['hpregenperlevel']
+
+    mpregen = championStats['mpregen']
+    mpregenperlevel = championStats['mpregenperlevel']
+
+    crit = championStats['crit']
+    critperlevel = championStats['critperlevel']
+
+    attackdamage = championStats['attackdamage']
+    attackdamageperlevel = championStats['attackdamageperlevel']
+
+    attackspeed = championStats['attackspeed']
+    attackspeedperlevel = championStats['attackspeedperlevel']
+
+    movespeed = championStats['movespeed']
+    attackrange = championStats['attackrange']
+
+    level = 18
+
+    print()
+    print(championName.upper()+ ", " +championTitle.title() + ":")
+    print(f"{championName} is a {championAttributes}")
+    print("A tip: ", end="")
+    print(championTip)
+    print("Champion Difficulty: " + str(championDifficulty) + "/10")
+    print()
